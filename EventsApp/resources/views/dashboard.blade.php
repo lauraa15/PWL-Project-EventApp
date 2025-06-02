3a5f98162e1e7dcc40e3d52ca6c6ce7a07cdf8ff
lauraa15 <laurapuspaa15@gmail.com>
2025-06-02 22:40:53 +0700

    push

--- EventsApp/resources/views/dashboard.blade.php
+++ EventsApp/resources/views/dashboard.blade.php
@@ -4,7 +4,8 @@
 
 @section('content')
 <div class="page-heading">
-    <h3>Profile Statistics</h3>
+    <h3>Welcome to Attendify !</h3>
+    <p class="text-subtitle text-muted">Your dashboard for managing events and attendees.</p>
 </div>
 <div class="page-content">
     <section class="row">
@@ -17,6 +18,23 @@
                                 <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                                     <div class="stats-icon purple mb-2">
                                         <i class="iconly-boldShow"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Profile Views</h6>
+                                    <h6 class="font-extrabold mb-0">112.000</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                    <div class="stats-icon purple mb-2">
+                                        <i class="fa-fw select-all fas"></i>
                                     </div>
                                 </div>
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
